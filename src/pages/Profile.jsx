41b7c0ebb2d72bec33f078509b9f494d21f3f857
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-15 09:17:56 +0100

    profile card

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,42 @@
 import React from "react";
-import FormUser from "../components/Forms/FormUser";
+import "../styles/profileCard.css";
+import SocialFollow from "../components/SocialFollow";
+import { withUser } from "../components/Auth/withUser";
 
 const Profile = (props) => {
+  console.log(props);
+
   return (
-    <div>
-      <h1>Protected profile</h1>
-      <FormUser />
+    <div className="Card">
+      <div className="upper-container">
+        <div className="image-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/1200px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"
+            alt="img"
+          />
+        </div>
+        <div className="lower-container">
+          <span> {props.context.user.following.length} following </span>
+          <span> {props.context.user.following.length} followers</span>
+        </div>
+        <div>
+          <SocialFollow />
+        </div>
+        <div>
+          <h3>Have someone write your letters of recommendation.</h3>
+          <p>
+            Give them plenty of time to write the letters. You should be
+            thinking well in advance aboutwhich teachers you want to write
+            recommendations.Give them plenty of time to write the letters. You
+            should be thinking well in advance aboutwhich teachers you want to
+            write recommendations.Give them plenty of time to write the letters.
+            You should be thinking well in advance aboutwhich teachers you want
+            to write recommendations.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Profile;
+export default withUser(Profile);
