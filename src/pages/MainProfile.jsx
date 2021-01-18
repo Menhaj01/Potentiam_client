0987a4ed8f3094964f55bade68a75842499be630
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-18 10:11:33 +0100

    main profile css

--- src/pages/MainProfile.jsx
+++ src/pages/MainProfile.jsx
@@ -12,14 +12,17 @@ export class MainProfile extends Component {
           <ProfileCard />
         </div>
         <div className="food-img">
-          <h1>Alicia cooking</h1>
           <div>
-            <span>FOOD</span>
-            <img
-              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-              alt="image"
-            />
-            <p>Yummy meal milly </p>
+            <h1>Alicia cooking</h1>
+            <div>
+              <span>FOOD</span>
+              <img
+                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                alt="image"
+              />
+
+              <p>Yummy meal milly </p>
+            </div>
           </div>
           <div className="food-img">
             <span>RUNNING</span>
