a2f594b729df51c5d8cd413497da8aaf06cc84c1
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-18 09:56:49 +0100

    profile css

--- src/pages/MainProfile.jsx
+++ src/pages/MainProfile.jsx
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
-import SocialFollow from "../components/SocialFollow";
+/* import SocialFollow from "../components/SocialFollow"; */
 import "../styles/profile.css";
 import ProfileCard from "../pages/ProfileCard";
 
 export class MainProfile extends Component {
   render() {
     return (
-      <div>
-        <ProfileCard />
-        <h1>Alicia cooking</h1>
-        <div>
-          <span>FOOD</span>
-          <img
-            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            alt="image"
-          />
-          <p>Yummy meal milly </p>
+      <div className="main-profile">
+        <div className="profile-card">
+          {" "}
+          <ProfileCard />
         </div>
-        <div>
-          <span>RUNNING</span>
-          <img
-            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            alt="image"
-          />
-          <p>Alicia cooking </p>
+        <div className="food-img">
+          <h1>Alicia cooking</h1>
+          <div>
+            <span>FOOD</span>
+            <img
+              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              alt="image"
+            />
+            <p>Yummy meal milly </p>
+          </div>
+          <div className="food-img">
+            <span>RUNNING</span>
+            <img
+              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              alt="image"
+            />
+            <p>Alicia cooking </p>
+          </div>
         </div>
       </div>
     );
